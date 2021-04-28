cef69c3455e3de8f3c779b19554a9382d26117b2
yoseham <1416708654@qq.com>
2021-04-28 12:41:42 +0800

    sum_edit

--- sum/sum.go
+++ sum/sum.go
@@ -1,8 +1,9 @@
 package sum
-func Sum(numbers [5]int) int{
-	res:=0
-	for i:=0; i<5; i++{
-		res+=numbers[i]
+
+func Sum(numbers []int) int {
+	res := 0
+	for _, number := range numbers {
+		res += number
 	}
 	return res
 }
