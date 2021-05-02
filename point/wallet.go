7c65bbc4a7fff6628fca0e22184dafb359fc2426
yoseham <1416708654@qq.com>
2021-05-02 19:59:19 +0800

    指针

--- point/wallet.go
+++ point/wallet.go
@@ -1,1 +1,35 @@
 package point
+
+import (
+	"errors"
+	"fmt"
+)
+
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Println("address of balance in Deposit is", &w.balance)
+	w.balance = amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return InsufficientFundsError
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
--- point/wallet_test.go
+++ point/wallet_test.go
@@ -1,1 +1,52 @@
 package point
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+	assertNoError := func(t *testing.T, got error) {
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want error) {
+		if got == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		fmt.Println("address of balance in test is", &wallet.balance)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(40)}
+		err := wallet.Withdraw(Bitcoin(20))
+		assertBalance(t, wallet, Bitcoin(20))
+		assertNoError(t, err)
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(40)}
+		err := wallet.Withdraw(Bitcoin(100))
+		assertBalance(t, wallet, Bitcoin(40))
+		assertError(t, err, InsufficientFundsError)
+	})
+
+}
