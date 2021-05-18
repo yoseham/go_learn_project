e0fae9d40ec26a7c40648afdac828d61dc4144ae
yoseham <1416708654@qq.com>
2021-05-18 17:25:27 +0800

    并发

---
+++ concurrency/website_test.go
@@ -0,0 +1,53 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func slowStuWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		urls[i] = "a url"
+	}
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStuWebsiteChecker, urls)
+	}
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	actualResults := CheckWebsites(mockWebsiteChecker, websites)
+	want := len(websites)
+	got := len(actualResults)
+	if want != got {
+		t.Fatalf("want %v, got %v", want, got)
+	}
+	expectedResults := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	if !reflect.DeepEqual(expectedResults, actualResults) {
+		t.Fatalf("want %v, got %v", want, got)
+	}
+}
