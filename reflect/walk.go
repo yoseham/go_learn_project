e2bcfe145169b28dc4c59c48c67a97cc7528daae
yoseham <1416708654@qq.com>
2021-05-19 17:32:18 +0800

    reflect

---
+++ reflect/walk.go
@@ -0,0 +1,35 @@
+package reflect
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+
+func getValue(x interface{}) (val reflect.Value) {
+	val = reflect.ValueOf(x)
+	switch val.Kind() {
+	case reflect.Ptr:
+		val = val.Elem()
+	}
+	return
+}
